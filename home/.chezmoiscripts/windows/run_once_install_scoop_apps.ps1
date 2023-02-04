452b8fe90e5def13d4300cf1c75aa6e789a1caaf
Tatsumasa Mihara <tatsumasa.mihara@gmail.com>
2023-02-05 08:42:13 +0900

    added scoop apps install script

--- home/.chezmoiscripts/windows/run_once_install_scoop_apps.ps1
+++ home/.chezmoiscripts/windows/run_once_install_scoop_apps.ps1
@@ -1,14 +1,41 @@
-# Utils
-# scoop install sudo git curl 7zip autohotkey windows-terminal
+if (Get-Command scoop -errorAction SilentlyContinue)
+{
+  $scoop_apps = @(
+    # Utils
+    'sudo',
+    'git',
+    'curl',
+    '7zip',
+    'autohotkey',
+    'windows-terminal',
 
-# Encryption
-# scoop install age bitwarden-cli
+    # Encryption
+    'age',
+    'bitwarden-cli',
 
-# Editors
-# scoop install vscode neovim
+    # Editors
+    'neovim',
+    'vscode',
 
-# CLI utils
-# scoop install starship fd fzf ripgrep
+    # CLI utils
+    'starship',
+    'fd',
+    'fzf',
+    'ripgrep',
 
-# Documentation
-# scoop install joplin sumatrapdf  
+    # Documentation
+    'joplin',
+    'sumatrapdf'
+    )
+
+  Write-Host "The following scoop apps will be installed:`n"
+  Write-Host $scoop_apps "`n"
+  $answer = Read-host "Do you want to continue? [Y/n] (default [n])"
+  if ($answer -eq 'Y') 
+  {
+    # Install apps above
+    scoop install $scoop_apps
+  }else{
+    Write-Host 'Abort.'
+  }
+}
