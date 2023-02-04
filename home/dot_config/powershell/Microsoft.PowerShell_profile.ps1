8f3a0c9f3bc967715ddacc4862386087d274dc15
Tatsumasa Mihara <tatsumasa.mihara@gmail.com>
2023-02-05 08:54:51 +0900

    added powershell aliases

--- home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,14 @@
-# Completion
-. $HOME/.config/powershell/chezmoi_completion.ps1
+# chezmoi completion
+if (Get-Command chezmoi -errorAction SilentlyContinue)
+{
+  . $HOME/.config/powershell/chezmoi_completion.ps1
+}
 
+# setup starship prompt
 if (Get-Command starship -errorAction SilentlyContinue)
 {
   Invoke-Expression (&starship init powershell)
 }
+
+# Aliases
+. $HOME/.config/powershell/aliases.ps1
---
+++ home/dot_config/powershell/aliases.ps1
@@ -0,0 +1,1 @@
+New-Alias -Name "ex" explorer
