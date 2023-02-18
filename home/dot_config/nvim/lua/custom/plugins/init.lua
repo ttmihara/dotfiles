0937085cc30a63a7d4e99528d81e2b729ecd8a08
Tatsumasa Mihara <tatsumasa.mihara@gmail.com>
2023-02-18 13:51:29 +0900

    added vim-fugitive

--- home/dot_config/nvim/lua/custom/plugins/init.lua
+++ home/dot_config/nvim/lua/custom/plugins/init.lua
@@ -1,12 +1,17 @@
 local M = {
   ["goolord/alpha-nvim"] = {
-    disable = false,
+    disable = true,
     opt = false,
   },
   ["folke/which-key.nvim"] = {
     disable = false,
     opt = false,
-  }
+  },
+  ["tpope/vim-fugitive"] = {
+    disable = false,
+    opt = true,
+    cmd = {'Git', 'G'}
+  },
 }
 
 return M
