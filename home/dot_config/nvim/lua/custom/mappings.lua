7e0b6583edd8f4413776aa9475ea16f2ddf52763
Tatsumasa Mihara <tatsumasa.mihara@gmail.com>
2023-05-21 20:56:04 +0900

    added null-ls config

--- home/dot_config/nvim/lua/custom/mappings.lua
+++ home/dot_config/nvim/lua/custom/mappings.lua
@@ -14,4 +14,32 @@ M.general = {
 
 }
 
+M.packer = {
+  n = {
+    ["<leader>ps"] = {"<cmd> PackerStatus <CR>", "PackerStatus"},
+    ["<leader>pS"] = {"<cmd> PackerSync <CR>", "PackerSync"},
+    ["<leader>pc"] = {"<cmd> PackerCompile <CR>", "PackerCompile"},
+    ["<leader>pC"] = {"<cmd> PackerClean <CR>", "PackerClean"},
+    ["<leader>pi"] = {"<cmd> PackerInstall <CR>", "PackerInstall"},
+    ["<leader>pl"] = {"<cmd> PackerLoad <CR>", "PackerLoad"},
+    ["<leader>pu"] = {"<cmd> PackerUpdate <CR>", "PackerUpdate"},
+  }
+}
+
+M.mason = {
+  n = {
+    ["<leader>mm"] = {"<cmd> Mason <CR>", "Mason"},
+    ["<leader>ml"] = {"<cmd> MasonLog <CR>", "MasonLog"},
+    ["<leader>mu"] = {"<cmd> MasonUpdate <CR>", "MasonUpdate"},
+  }
+}
+
+M.LSP = {
+  n = {
+    ["<leader>li"] = {"<cmd> LspInfo <CR>", "LspInfo"},
+    ["<leader>lr"] = {"<cmd> LspRestart <CR>", "LspRestart"},
+    ["<leader>ll"] = {"<cmd> LspLog <CR>", "LspLog"},
+  }
+}
+
 return M
---
+++ home/dot_config/nvim/lua/custom/plugins/null-ls.lua
@@ -0,0 +1,17 @@
+local null_ls = require("null-ls")
+
+local formatting = null_ls.builtins.formatting
+-- local lint = null_ls.builtins.diagnostics
+
+local sources = {
+	formatting.clang_format,
+	formatting.cmake_format,
+	formatting.stylua,
+	formatting.prettier,
+	formatting.xmllint,
+}
+
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
