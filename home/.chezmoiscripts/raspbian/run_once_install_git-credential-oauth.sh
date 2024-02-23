c4f65ac0f54a482170d707e3bc4091dedaf551f1
Tatsumasa Mihara <tatsumasa.mihara@gmail.com>
2024-02-23 12:54:33 +0900

    modified git-credential-install script

--- home/.chezmoiscripts/raspbian/run_once_install_git-credential-oauth.sh
+++ home/.chezmoiscripts/raspbian/run_once_install_git-credential-oauth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl -LO https://github.com/hickford/git-credential-oauth/releases/download/v0.11.0/git-credential-oauth_0.11.0_linux_arm64.tar.gz
-mkdir -p git-credential-oauth
+mkdir -pv git-credential-oauth
 tar -xzvf git-credential-oauth_0.11.0_linux_arm64.tar.gz -C git-credential-oauth
-mkdir -p ~/bin
-mv -n git-credential-oauth/git-credential-oauth ~/bin/
-rm -rf git-credential-oauth*
+mkdir -pv ~/bin
+mv -nv git-credential-oauth/git-credential-oauth ~/bin/
+rm -rfv git-credential-oauth*
