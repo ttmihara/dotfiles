65614b7edd99ff79fc38bc9ccde9853e6c32f89a
Tatsumasa Mihara <tatsumasa.mihara@gmail.com>
2024-02-23 21:24:40 +0900

    upgrade powershell experience

--- home/dot_config/powershell/aliases.ps1
+++ home/dot_config/powershell/aliases.ps1
@@ -1,6 +1,11 @@
 # Aliases
-New-Alias -Name "e" explorer
-New-Alias -Name "cz" chezmoi
+New-Alias -Name "e" explorer -errorAction SilentlyContinue
+New-Alias -Name "cz" chezmoi -errorAction SilentlyContinue
 
 # KeyHandler
-Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadlineKeyHandler -Chord ctrl+d -Function ViExit
+Set-PSReadlineKeyHandler -Chord ctrl+u -Function BackwardKillLine
+Set-PSReadLineKeyHandler -Chord UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Chord ctrl+n -Function NextHistory
+Set-PSReadlineKeyHandler -Chord ctrl+p -Function PreviousHistory
